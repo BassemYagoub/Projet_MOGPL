0cc93264161c2d9b6ad89a22fa0d71d99dc52bca
BassemYagoub <yagoub.bassem@gmail.com>
2020-12-29 18:38:58 +0100

    Debut part2

--- src/localisation_soins_patientsQ2_1.py
+++ src/localisation_soins_patientsQ2_1.py
@@ -32,63 +32,69 @@ if __name__ == "__main__":
     
     k = 3
     alpha = 0.2
-    villes_soins = [i for i in range(0, k)] #indice des villes choisies arbitrairement
 
     gamma = gamma_val(alpha, k, populations)
     print("gamma =", gamma, "\nalpha =", alpha,", k =", k)
 
     
-    nbcont = k+len(dist_matrice)
+    nbcont = len(dist_matrice)*2+1
     lignes = range(nbcont)
     
-    nbvar = len(dist_matrice)*k
+    nbvar = (len(dist_matrice)*len(dist_matrice))+len(dist_matrice)
     colonnes = range(nbvar)
     
-    #sous-matrice n*k car le reste est inintéressant pour le PL
-    dist_sous_matrice = dist_matrice[:, :k]
-    
-    for row in dist_sous_matrice:
+    for row in dist_matrice:
         print(row.tolist())
     print(populations,"\n")
+    
     # Matrice des contraintes
     #Contrainte 1
     a = []
-    for i in range(k):
+    for i in range(len(dist_matrice)):
         a.append([0]*nbvar)
         for j in range(len(populations)):
-            a[i][j+(i*len(dist_sous_matrice))] = populations[j]
-            
+            a[i][j+(i*len(dist_matrice))] = populations[j]
     
-    #Contrainte 2 (= toute la sous matrice n*k)
-    for i in range(len(dist_sous_matrice)):
+    
+    #Contrainte 2 (= toute la matrice)
+    for i in range(len(dist_matrice)):
         a.append([0]*nbvar)
-        for j in range(k):
+        for j in range(len(dist_matrice)):
             #i+k pour ne pas ecraser les k Contrainte1
-            a[i+k][i+j*len(dist_sous_matrice)] = 1
+            a[i+len(dist_matrice)][i+j*len(dist_matrice)] = 1
+    
+    
+    #Contrainte 3 yj = k
+    a.append([0]*nbvar)
+    for i in range(nbvar-len(dist_matrice), nbvar):
+        print(i)
+        a[nbcont-1][i] = 1
+        
     
     for row in a:
         print(row)
     
     # Second membre
-    b = [gamma]*k+[1]*(nbcont-k)
+    b = [gamma]*len(dist_matrice)+[1]*len(dist_matrice)+[k]
     print("SM\n", b)
     
     # Coefficients de la fonction objectif
-    coefficients_dij = []
-    for ville in villes_soins:
-        coefficients_dij += dist_sous_matrice[:, ville:ville+1].flatten().tolist()
+    coefficients_dij = dist_matrice.flatten().tolist()
+    coefficients_dij += [0]*len(dist_matrice)
         
     print("COEFFS\n", coefficients_dij, "\n")
     
     c = coefficients_dij
     
-    m = Model("localisation_soins")     
+    m = Model("localisation_soinsQ2_1")     
     
     # declaration variables de decision
     x = []
-    for i in range(nbvar):
+    for i in range(nbvar-len(dist_matrice)):
         x.append(m.addVar(vtype=GRB.BINARY, lb=0, ub=1, name="x"+str(i)))
-
+    for j in range(len(dist_matrice)):
+        x.append(m.addVar(vtype=GRB.BINARY, lb=0, ub=1, name="y"+str(j)))
+    
     # maj du modele pour integrer les nouvelles variables
     m.update()
     
@@ -105,7 +111,7 @@ if __name__ == "__main__":
     for i in lignes:
         if(cpt < k):
             m.addConstr(quicksum(a[i][j]*x[j] for j in colonnes) <= b[i], "Contrainte%d " % i)
-        else:
+        elif(cpt < len(lignes)-1):
             m.addConstr(quicksum(a[i][j]*x[j] for j in colonnes) == b[i], "Contrainte%d " % i)
         cpt+=1
     
@@ -114,26 +120,26 @@ if __name__ == "__main__":
     
                     
     print('\nSolution optimale:')
+    """
     for i in range(int(nbvar/k)):
         for j in range(k):
-            print(int(x[i+(j*len(dist_sous_matrice))].x), end=' ')
+            print(int(x[i+(j*len(dist_matrice))].x), end=' ')
         print("\n", end='')
         
-    print('\nValeur de la fonction objectif :', m.objVal/len(dist_sous_matrice))
+    print('\nValeur de la fonction objectif :', m.objVal/len(dist_matrice))
     
-res = []
-tmp = []
-for i in range(len(populations)*k):
-    tmp.append(x[i].x)
-    if len(tmp)==len(populations):
-        res.append(tmp)
-        tmp=[]
-
-di=0
-for i in range(len(res)):
-    for j in range(len(res[0])):
-        di+=dist_sous_matrice[j][i]*res[i][j]*populations[j]
-di/=sum(populations)
-
-print(di)
+    res = []
+    tmp = []
+    for i in range(len(populations)*k):
+        tmp.append(x[i].x)
+        if len(tmp)==len(populations):
+            res.append(tmp)
+            tmp=[]
     
+    di=0
+    for i in range(len(res)):
+        for j in range(len(res[0])):
+            di+=dist_matrice[j][i]*res[i][j]*populations[j]
+    di/=sum(populations)
+    
+    print(di)"""
