77e68e9151e1203d95a09889b578259332f6bfc5
BassemYagoub <yagoub.bassem@gmail.com>
2020-12-30 17:34:38 +0100

    2_1 fonctionnelle

--- src/localisation_soins_patientsQ2_1.py
+++ src/localisation_soins_patientsQ2_1.py
@@ -10,12 +10,13 @@ import numpy as np
 def importCSV(csv_name):
     """importe le csv d'un ensemble de villes et le retourne dans une matrice+vecteur"""
     dist_matrice = np.genfromtxt(csv_name, delimiter=';', dtype=int, filling_values=0)
+    noms_villes = np.genfromtxt(csv_name, delimiter=';', dtype=str, filling_values=0)[0, 1:]
     populations = dist_matrice[1:, 0]
-    
+    print(noms_villes)
     dist_matrice = dist_matrice[1:, 2:] #on enleve les nom de lignes/colonnes + la pop
     dist_matrice += dist_matrice.T      #on remplie les cases vides par symétrie
     
-    return dist_matrice, populations
+    return dist_matrice, populations, noms_villes
 
 
 def gamma_val(alpha, k, populations):
@@ -27,7 +28,7 @@ def gamma_val(alpha, k, populations):
 
 if __name__ == "__main__":
     #valeurs et constantes necessaires au PL
-    dist_matrice, populations = importCSV('../ressources/villes.csv')
+    dist_matrice, populations, noms_villes = importCSV('../ressources/villes.csv')
     
     k = 3
     alpha = 0.2
@@ -36,36 +37,21 @@ if __name__ == "__main__":
     print("gamma =", gamma, "\nalpha =", alpha,", k =", k)
 
     n = len(dist_matrice)
-    nbcont = len(dist_matrice)*2+1
-    lignes = range(nbcont)
-    
-    nbvar = (n*n)+n
-    colonnes = range(nbvar)
     
     for row in dist_matrice:
         print(row.tolist())
     print(populations,"\n")
-
-    # Second membre
-    b = [gamma]*n+[1]*n+[k]
-    print("SM\n", b)
-    
-    # Coefficients de la fonction objectif
-    coefficients_dij = dist_matrice.flatten().tolist()
-    coefficients_dij += [0]*n
-        
-    print("COEFFS\n", coefficients_dij, "\n")
-    
-    c = coefficients_dij
     
     m = Model("localisation_soinsQ2_1")     
     
     # declaration variables de decision
     x_temp = []
+    ind = 0
     for i in range(n):
         x_temp.append([])
         for j in range(n):
-            x_temp[i].append(m.addVar(vtype=GRB.BINARY, lb=0, ub=1, name="x"+str(i)))
+            x_temp[i].append(m.addVar(vtype=GRB.BINARY, lb=0, ub=1, name="x"+str(ind)))
+            ind += 1
     
     x = np.array(x_temp)
             
@@ -76,59 +62,38 @@ if __name__ == "__main__":
     # maj du modele pour integrer les nouvelles variables
     m.update()
     
-    obj = LinExpr();
-    obj = 0
-    cpt=0
-    for i in range(n):
-        for j in range(n):
-            obj += c[cpt] * x[i][j]
-            cpt+=1
-            
-    for j in range(n):
-        obj += c[cpt] * y[j]
-        cpt+=1
             
     # definition de l'objectif
+    obj = LinExpr();
+    obj = 0
+    for i in range(n):
+        for j in range(n):
+            obj += dist_matrice[i][j] * x[i][j] * populations[i]
+    obj /= sum(populations)
+
     m.setObjective(obj,GRB.MINIMIZE)
     
-    # Definition des contraintes  
-    
-    m.addConstr(np.sum(y) == k)
+    # Definition des contraintes  (de C1 à C3)
     for j in range(n):
-        m.addConstr(np.dot(x[:,j], populations) <= (y[j]*((1+alpha)/k)*np.sum(populations)))
+        m.addConstr(np.dot(x[:,j], populations) <= (y[j]*gamma))
     
     for i in range(n):
         m.addConstr(np.sum(x[i,:]) == 1, "Contrainte%d" % (k+i))
+        
+    m.addConstr(np.sum(y) == k) 
     
     # Resolution
     m.optimize()
     
              
-    print('\nSolution optimale:')
-    
-    for i in range(n):
-        for j in range(n):
-            print(int(x[i][j].x), end=' ')
-        print("\n", end='')
-    
-    print("y :")
+    print('\Affectation optimale:\n')
     for j in range(n):
-        print(int(y[j].x), end=' ')
+        if(int(y[j].x) == 1):
+            print("Secteur",noms_villes[j+1], end=':\n\t')
+            for i in range(n):
+                if(int(x[:, j][i].x)) == 1:
+                    print(noms_villes[i+1], end=', ')
+            print("\n")
     
     print('\nValeur de la fonction objectif :', m.objVal)
     
-    res = []
-    tmp = []
-    for i in range(n):
-        for j in range(n):
-            tmp.append(x[i][j].x)
-            if len(tmp)==len(populations):
-                res.append(tmp)
-                tmp=[]
-    
-    di=0
-    for i in range(len(res)):
-        for j in range(len(res[0])):
-            di+=dist_matrice[j][i]*res[i][j]*populations[j]
-    di/=sum(populations)
-    print(di)
