80698e4673928892ce1920cdd12468bc8f671661
RICHAUME ANTHEA <anthea.richaume@etu.upmc.fr>
2021-01-02 21:07:11 +0100

    Q3_2 OK

--- src/equilibrage_unite.py
+++ src/equilibrage_unite.py
@@ -24,33 +24,24 @@ if __name__ == "__main__":
     dist_matrice, populations, noms_villes = importCSV('../ressources/villes.csv')
     
     # Matrice des coûts (distance entre unité i et unité j)
-    d = dist_matrice[:5]
-    n = 7
-    b = [150, 150, 25, 150, 25]
-    #b = [100, 100, 100, 100, 100]
+    n = 5    
+    d = dist_matrice[:n]
+
+    #Instances à tester
+    #b = [150, 150, 25, 150, 25]
+    #b = [90, 50, 130, 60, 150]
+    b = [40, 160, 100, 80, 50]
     
     m = Model("equilibrage_uniteQ3_1")
     
     # declaration variables de decision
     x_temp = []
-    ind = 0
-    for i in range(5):
+    for i in range(n):
         x_temp.append([])
-        for j in range(5):
+        for j in range(n):
             x_temp[i].append(m.addVar(vtype=GRB.CONTINUOUS, lb=0, name="x"+str(i)+str(j)))
-            ind += 1
     
     x = np.array(x_temp)
-    
-    y_temp = []
-    ind = 0
-    for i in range(5):
-        y_temp.append([])
-        for j in range(5):
-            y_temp[i].append(m.addVar(vtype=GRB.CONTINUOUS, lb=0, name="y"+str(i)+str(j)))
-            ind += 1
-    
-    y = np.array(y_temp)
     
     # maj du modele pour integrer les nouvelles variables
     m.update()
@@ -58,34 +49,38 @@ if __name__ == "__main__":
     # definition de l'objectif
     obj = LinExpr();
     obj = 0
-    for i in range(5):
-        for j in range(5):
+    for i in range(n):
+        for j in range(n):
             obj += d[i][j] * x[i][j]
 
     m.setObjective(obj,GRB.MINIMIZE)
     
-    #Definition des contraintes    
-    for i in range(5):            
-        m.addConstr(np.sum(x[i,:]) - np.sum(y[i,:]) == b[i])
+    #Definition des contraintes       
+    for i in range(n):            
+        m.addConstr(np.sum(x[i,:]) == b[i])
         
-    for i in range(5):
-        m.addConstr(np.sum(y[i,:]) - np.sum(x[i,:]) == -100)
+    for i in range(n):
+        m.addConstr(np.sum(x[:,i]) <= 100)
             
     
-     # Resolution
+    # Resolution
     m.optimize()
     
     print('\nValeur de la fonction objectif :', m.objVal)
+            
+    res=[]
+    tmp=[]
     
-    for i in range(5):
-        print('\n')
-        for j in range(5):
-            print(int(x[i][j].x), end=' ')
-            
-            
-    for i in range(5):
-        print('\n')
-        for j in range(5):
-            print(int(y[i][j].x), end=' ')
+    for i in range(n):
+        for j in range(n):
+            tmp.append(x[i][j].x)
+
+        res.append(tmp)
+        tmp=[]
+    np.set_printoptions(edgeitems=15)
+    res=np.asmatrix(res)
+    
+    print("Répartition des patients : ")
+    print(res.T)
     
     
